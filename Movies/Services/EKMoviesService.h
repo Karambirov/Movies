443616942ac8d39380a15dd350ea42eaea4307b2
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-03-29 00:03:33 +0300

    Create EKMoviesService

---
+++ Movies/Services/EKMoviesService.h
@@ -0,0 +1,17 @@
+//
+//  EKMoviesService.h
+//  Movies
+//
+//  Created by Eugene Karambirov on 29/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EKMoviesService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Movies/Services/EKMoviesService.m
@@ -0,0 +1,13 @@
+//
+//  EKMoviesService.m
+//  Movies
+//
+//  Created by Eugene Karambirov on 29/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+#import "EKMoviesService.h"
+
+@implementation EKMoviesService
+
+@end

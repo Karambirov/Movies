d705e871452489a23995d56fb25f805340934f1a
Eugene Karambirov <eugenekarambirov@gmail.com>
2019-03-29 12:13:34 +0300

    Create MovieTableViewCell

---
+++ Movies/Modules/Cells/MovieTableViewCell/MovieTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  MovieTableViewCell.h
+//  Movies
+//
+//  Created by Eugene Karambirov on 29/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MovieTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Movies/Modules/Cells/MovieTableViewCell/MovieTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.m
+//  Movies
+//
+//  Created by Eugene Karambirov on 29/03/2019.
+//  Copyright © 2019 Eugene Karambirov. All rights reserved.
+//
+
+#import "MovieTableViewCell.h"
+
+@implementation MovieTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
